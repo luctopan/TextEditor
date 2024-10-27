251200e5f943be982dee009e5f9fdd5ca4a1039a
Alex Luciano <alex.luciano.topan@gmail.com>
2024-10-27 20:32:57 -0300

    finalizando o projeto

--- obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
+++ obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b17e032849ee09dbc41422265f8322d68f0bed9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d997cde0af3b81f9863e5ceb61a80b7bb579894e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
